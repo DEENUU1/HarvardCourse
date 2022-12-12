32889d24afb865fca7191c5c5e1a127db82a14fd
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-12-12 18:54:19 +0100

    Problem Set 1 [1/5]

---
+++ deep.py
@@ -0,0 +1,8 @@
+# https://cs50.harvard.edu/python/2022/psets/1/deep/
+
+user_input = input('What is the Answer to the Great Question of Life, the Universe, and Everytking? ')
+
+if user_input == '42' or user_input == 'forty-two' or user_input == 'forty two':
+    print('Yes')
+else:
+    print('No')
